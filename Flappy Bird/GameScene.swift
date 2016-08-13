8b551c750a98b82590a4f4c0f4d2fef305ec4f25
Nadeem Ansari <nadeemansari@nadeems-macbook-air.local>
2016-08-13 16:00:59 -0500

    Added bird sprite to main screen.

--- Flappy Bird/GameScene.swift
+++ Flappy Bird/GameScene.swift
@@ -9,34 +9,42 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var bird = SKSpriteNode()
+    
+    var bg = SKSpriteNode()
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+
+        let birdTexture = SKTexture(imageNamed: "flappy1.png")
+        let birdTexture2 = SKTexture(imageNamed: "flappy2.png")
         
-        self.addChild(myLabel)
+        let animation = SKAction.animateWithTextures([birdTexture,birdTexture2], timePerFrame: 0.1)
+        
+        let makeBirdFlap = SKAction.repeatActionForever(animation)
+        
+        bird = SKSpriteNode(texture: birdTexture)
+        
+        bird.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame))
+        
+        bird.runAction(makeBirdFlap)
+        
+        self.addChild(bird)
+        
+        let bgTexture = SKTexture(imageNamed: "bg.png")
+        
+        
+        
+        
+        
+        
+        
+        
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
         
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
     }
    
     override func update(currentTime: CFTimeInterval) {
